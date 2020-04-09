8b95c1b995864d25b27dae3549c98647347c6ee0
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-04-09 19:27:56 +0100

    Added LA name list and explanatory footnotes

--- ui.R
+++ ui.R
@@ -1,7 +1,3 @@
-library(shiny)
-library(ggplot2)
-library(sf)
-
 ui <- fluidPage(
   
   #Set up title
@@ -11,9 +7,13 @@ ui <- fluidPage(
   sidebarLayout(
     
     sidebarPanel("User inputs",
-                 textInput("userLA" , "Select LA to map" , "Sheffield"),
-                 #textInput("userLAname", "Select LA name for map title", "Sheffield"),
+                 textInput("userLA" , "Select LA or LAs to map. Separate multiple LAs with :" , "Sheffield"),
+                 textInput("userLAname", "Select area name for map title", "Sheffield"),
+                 p("For a list of LA names click ",
+                   a("here", href="https://github.com/VictimOfMaths/COVIDmapper/blob/master/LAlist.csv")),
+                 #br(),
                  checkboxInput("LSOABoundaries", "Show LSOA boundaries", value=TRUE),
+                 #br(),
                  radioButtons("legendpos", "Select legend position", choices=list("Top left"=1,
                                                                                   "Top right"=2,
                                                                                   "Bottom left"=3,
@@ -21,8 +21,7 @@ ui <- fluidPage(
                                                                                   "No legend"=5),
                               selected=3),
                  actionButton("run", "Generate plot"),
-                 #downloadButton('downloadPlot', 'Download map'),
-                 width=2),
+                 width=3),
     
     #Set up main panel for the plot
     mainPanel(
@@ -30,5 +29,13 @@ ui <- fluidPage(
       br(),
       fluidRow(downloadButton('covid_download'))
     ) # end mainpanel
-  )
+  ),
+  hr(),
+  p("Health deprivation taken from the health component of the Index of Multiple Deprivation. Age-sex risk based on age-specific Infection Fatality Rates from ", 
+        a("Imperial College modelling,", 
+        href="https://www.imperial.ac.uk/media/imperial-college/medicine/sph/ide/gida-fellowships/Imperial-College-COVID19-NPI-modelling-16-03-2020.pdf"), 
+    "adapted to estimated age-sex-specific rates using male:female outcome ratios from Italy from ",
+    a("Istituto Superiore di Sanità.", href="https://www.epicentro.iss.it/coronavirus/bollettino/Bollettino-sorveglianza-integrata-COVID-19_26-marzo%202020.pdf"),
+  "This analysis assumes 100% infection rates and therefore represents the maximum potential mortality exposure of each area. Approach adapted from and inspired by the work of ",
+    a("Ilya Kashnitsky and José Aburto", href="https://doi.org/10.31219/osf.io/abx7s"))
 )
